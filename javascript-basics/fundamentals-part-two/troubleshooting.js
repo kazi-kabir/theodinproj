f833b7128ab0250d1783f66c425d3f31e7d70e20
kazi-kabir <kazi.s221999@gmail.com>
2023-05-16 17:10:40 +1000

    gg ez

---
+++ javascript-basics/fundamentals-part-two/troubleshooting.js
@@ -0,0 +1,15 @@
+function troubleshooting() {
+	const a = 1;
+	const b = 1;
+
+	let result;
+
+	// Edit between these lines
+	// =================================
+	result = a + b;
+
+	// =================================
+
+	return result;
+
+}
