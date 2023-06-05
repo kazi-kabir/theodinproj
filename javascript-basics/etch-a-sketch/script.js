dcd239f62e8123a76720e103f82d6e7a95069fce
kazi-kabir <kazi.s221999@gmail.com>
2023-06-05 19:42:42 +0600

    fooey commit removed

--- javascript-basics/etch-a-sketch/script.js
+++ javascript-basics/etch-a-sketch/script.js
@@ -4,7 +4,6 @@ container.innerHTML =
     `<div class="row">${'<div class="cell" onmouseover=changeDivColour()> </div>'.repeat(n)}</div>`.repeat(n);
 
 function changeDivColour() {
-    console.log('fooey')
     document.getElementsByClassName('cell')[i].style.backgroundColor = 'red';
 }
 
