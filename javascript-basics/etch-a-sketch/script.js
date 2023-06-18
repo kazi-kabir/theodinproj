cac13aeac349d63679ee45a0c33bf216aed6c7b9
kazi-kabir <kazi.s221999@gmail.com>
2023-06-18 23:13:19 +0600

    remove console

--- javascript-basics/etch-a-sketch/script.js
+++ javascript-basics/etch-a-sketch/script.js
@@ -4,7 +4,7 @@ container.innerHTML =
     `<div class="row">${'<div class="cell" onmouseover=changeDivColour()> </div>'.repeat(n)}</div>`.repeat(n);
 
 function changeDivColour() {
-    console.log('hovering over div here')
+    let foo = document.getElementsByClassName('cell')
     document.getElementsByClassName('cell')[i].style.backgroundColor = 'red';
 }
 
