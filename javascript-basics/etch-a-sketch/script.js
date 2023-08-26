a06d455e15e6a9999a2fb744513f9ea687037907
kazi-kabir <kazi.s221999@gmail.com>
2023-08-26 15:56:18 +1000

    IT'S HAPPENINGgit add .
    git st
    /

--- javascript-basics/etch-a-sketch/script.js
+++ javascript-basics/etch-a-sketch/script.js
@@ -2,7 +2,9 @@ let i=0, n=16;
 
 function myFunc(id) {
     console.log('myfunc')
-    console.log(this.id)
+    console.log(id)
+    let node = document.getElementById(id);
+    let content = console.log(node.innerHTML)
     document.getElementById(id).classList.add("cellHover");
 }
 
@@ -11,9 +13,24 @@ function uid() {
 }
 
 container.innerHTML = 
-    `<div class="row">${'<div id=uid() onmouseover="myFunc(this.id)" class="cell">X</div>'.repeat(n)}</div>`
-    .repeat(n).replace(/X/g,_=> (i++).toString(n))
+    `<div class="row">
+    ${'<div class="cell"> </div>'.repeat(n)}</div>`
+    .repeat(n);
 
+function getElements() {
+    let allElements = document.getElementsByClassName("cell");
+    console.log(allElements)
+    console.log(allElements[2])
+
+    for(let i = 0; i < allElements.length; i++) {
+        allElements[i].setAttribute('id', i + 1);
+        allElements[i].onmouseover = function(e) {
+            myFunc(allElements[i].id)
+        }
+    }
+}
+
+getElements();
 
 
 
