8c4049d1f1bcc53ef373b0f739c6f1529235dae3
kazi-kabir <kazi.s221999@gmail.com>
2023-08-26 12:33:47 +1000

    foo

--- javascript-basics/etch-a-sketch/README.md
+++ javascript-basics/etch-a-sketch/README.md
@@ -4,6 +4,11 @@ x make grid
   x trigger an event when mouseover
   - change div colour on mouseover
     - give each div a unique ID 
+    - pass the ID to my func 
+
+  - alt approach
+    - give each div some text inside like the original solution
+    - use css to hide the text
 
 - clear button
 
--- javascript-basics/etch-a-sketch/script.js
+++ javascript-basics/etch-a-sketch/script.js
@@ -1,15 +1,18 @@
 let i=0, n=16;
 
-function myFunc() {
+function myFunc(id) {
     console.log('myfunc')
-    document.getElementById('mydiv').classList.add('cellHover')
+    console.log(this.id)
+    document.getElementById(id).classList.add("cellHover");
+}
+
+function uid() {
+    return Math.random().toString(36);
 }
 
 container.innerHTML = 
-    `<div class="row">
-    ${'<div id="mydiv" onmouseover="myFunc()" class="cell"> </div>'.repeat(n)}</div>`
-    .repeat(n);
-
+    `<div class="row">${'<div id=uid() onmouseover="myFunc(this.id)" class="cell">X</div>'.repeat(n)}</div>`
+    .repeat(n).replace(/X/g,_=> (i++).toString(n))
 
 
 
