97c2025948310c01bf2582808314ae17b4d2f993
kazi-kabir <kazi.s221999@gmail.com>
2023-08-27 13:42:05 +1000

    added basic funcs

---
+++ javascript-basics/calculator/script.js
@@ -0,0 +1,15 @@
+function add(first, second) {
+    return first + second;
+}
+
+function subtract(first, second) {
+    return first - second;
+} 
+
+function divide(first, second) {
+    return first / second;
+} 
+
+function multiply(first, second) {
+    return first * second;
+} 
