655121efd0d7ed83616cf68cb113e9dc0f86ec7a
kazi-kabir <kazi.s221999@gmail.com>
2023-11-26 13:44:10 +1100

    fooey

--- fullstack-js/int-html-css/sign-up-form-project/assets/script.js
+++ fullstack-js/int-html-css/sign-up-form-project/assets/script.js
@@ -1,1 +1,15 @@
 console.log('hello')
+
+// TODO
+// get both passwords somehow and render them to console 
+// then find a matching algo online
+
+password = document.getElementById('pass')
+
+console.log(password)
+
+function comparePasswords() {
+    console.log(password)
+}
+
+
