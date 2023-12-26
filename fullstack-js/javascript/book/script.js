d86e54e91ae4e8243884a67af53d65ccc4f024e7
kazi-kabir <kazi.s221999@gmail.com>
2023-12-27 00:03:23 +1100

    replaced with form

--- fullstack-js/javascript/book/script.js
+++ fullstack-js/javascript/book/script.js
@@ -53,14 +53,82 @@ function addRowToTable() {
 }
 
 function submitBook() {
-    const newDiv = document.createElement("div");
+    const tableDiv = document.getElementById("myTable");
 
     // and give it some content
     const newContent = document.createTextNode("Hi there and greetings!");
 
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
+    // create submit form 
+    tableDiv.innerHTML = " "
 
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
+    var br = document.createElement("br"); 
+
+    // Create a form dynamically
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "submit.php");
+ 
+    // Create an input element for Full Name
+    var FN = document.createElement("input");
+    FN.setAttribute("type", "text");
+    FN.setAttribute("name", "FullName");
+    FN.setAttribute("placeholder", "Full Name");
+ 
+     // Create an input element for date of birth
+     var DOB = document.createElement("input");
+     DOB.setAttribute("type", "text");
+     DOB.setAttribute("name", "dob");
+     DOB.setAttribute("placeholder", "DOB");
+ 
+     // Create an input element for emailID
+     var EID = document.createElement("input");
+     EID.setAttribute("type", "text");
+     EID.setAttribute("name", "emailID");
+     EID.setAttribute("placeholder", "E-Mail ID");
+ 
+      // Create an input element for password
+      var PWD = document.createElement("input");
+      PWD.setAttribute("type", "password");
+      PWD.setAttribute("name", "password");
+      PWD.setAttribute("placeholder", "Password");
+ 
+       // Create an input element for retype-password
+       var RPWD = document.createElement("input");
+       RPWD.setAttribute("type", "password");
+       RPWD.setAttribute("name", "reTypePassword");
+       RPWD.setAttribute("placeholder", "ReEnter Password");
+ 
+                // create a submit button
+                var s = document.createElement("input");
+                s.setAttribute("type", "submit");
+                s.setAttribute("value", "Submit");
+                 
+                // Append the full name input to the form
+                form.appendChild(FN); 
+                 
+                // Inserting a line break
+                form.appendChild(br.cloneNode()); 
+                 
+                // Append the DOB to the form
+                form.appendChild(DOB); 
+                form.appendChild(br.cloneNode()); 
+                 
+                // Append the emailID to the form
+                form.appendChild(EID); 
+                form.appendChild(br.cloneNode()); 
+                 
+                // Append the Password to the form
+                form.appendChild(PWD); 
+                form.appendChild(br.cloneNode()); 
+                 
+                // Append the ReEnterPassword to the form
+                form.appendChild(RPWD); 
+                form.appendChild(br.cloneNode()); 
+                 
+                // Append the submit button to the form
+                form.appendChild(s); 
+ 
+                document.getElementsByTagName("body")[0]
+               .appendChild(form);
+
 }
