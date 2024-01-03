b778e959dd1972c6382e01e9e1ac8bbb52782b8a
kazi-kabir <kazi.s221999@gmail.com>
2024-01-03 21:33:34 +1100

    cleaned it

--- fullstack-js/javascript/book/script.js
+++ fullstack-js/javascript/book/script.js
@@ -21,25 +21,8 @@ function addBookToLibrary(book) {
 const TheHobbit = new Book("The Hobbit", "JK Rowling", "200", true)
 
 function addRow(tableID) {
-    console.log('inside addRow func')
-
-      // Get a reference to the table
-  let tableRef = document.getElementById(tableID);
-
-  // Insert a row at the end of the table
-  let newRow = tableRef.insertRow(-1);
-
-  // Insert a cell in the row at index 0
-  let newCell = newRow.insertCell(0);
-
-  // Append a text node to the cell
-  let newText = document.createTextNode("New bottom row");
-  newCell.appendChild(newText);
-    
 
 }
-
-addRow("myTable")
 
 console.log(myLibrary);
 
