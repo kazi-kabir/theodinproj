3ee04dc2052a4fe46dcc03e215db9b0af49aee77
kazi-kabir <kazi.s221999@gmail.com>
2024-01-06 13:42:37 +1100

    less is more baby

--- fullstack-js/javascript/book/script.js
+++ fullstack-js/javascript/book/script.js
@@ -59,6 +59,14 @@ function addRow(tableID) {
     console.log(tableStatus);
 
     console.log(tableID);
+
+    // also add button that hides this view and returns us to the main view
+    //make a form that takes in all the book object parameters
+    //create a new object from that form
+    //add that to our Library
+    // once submit happens return to main view to show all books
+
+
 }
 
 
