5b46daddcefa96e32a8a571b408b8f6a5387a475
kazi-kabir <kazi.s221999@gmail.com>
2024-02-02 18:59:54 +1100

    basic func

---
+++ fullstack-js/javascript/tic-tac-toe/README.md
@@ -0,0 +1,17 @@
+tictactoe LOGIC
+
+- the board is 12345689 combinations of
+- 123
+- 456
+- 789
+- winning combos are
+  - 123
+  - 456
+  - 789
+  - 147
+  - 258
+  - 369
+  - 159
+  - 357 
+  - and those reversed 
+
--- fullstack-js/javascript/tic-tac-toe/script.js
+++ fullstack-js/javascript/tic-tac-toe/script.js
@@ -1,17 +1,35 @@
-console.log('test')
-
-class GameBoard {
-    
-    drawGameBoard() {
-
-        // draw the gameboard here
-
-
-    }
-}
-
-
 // tictactoe logic
 // 3x3 grid
 // 3 in a row to win 
 // multidimensional array?
+// winnings codes
+
+console.log('hello world')
+
+let correctArray = [123];
+let reversedCorrectArray = [321];
+let incorrectArray = [312];
+
+
+function isWinner(playerArray) {
+
+    const winningArrays = [[123], [456], [789], [147], [258], [369], [159], [357]]
+
+    winningArrays.forEach((i) => {
+        console.log(i)
+        if(JSON.stringify(i) === JSON.stringify(playerArray)) {
+            console.log('yeet')
+        }
+
+        let reversedPlayerArray = playerArray.reverse();
+
+        console.log(reversedPlayerArray)
+
+        if(JSON.stringify(i) === JSON.stringify(reversedPlayerArray)) {
+            console.log('reversed yeet')
+        }
+    })
+
+}
+
+isWinner(correctArray)
