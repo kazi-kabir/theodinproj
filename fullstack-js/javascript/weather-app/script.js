9a6b2b97860c76cd2d8aa5189d31bff51b172071
kazi-kabir <kazi.s221999@gmail.com>
2024-02-16 08:23:18 +1100

    weather app working

---
+++ fullstack-js/javascript/weather-app/script.js
@@ -0,0 +1,23 @@
+console.log('hello')
+
+let temperature = document.getElementById('temperature');
+let weatherLocation = document.getElementById('location');
+
+async function getWeatherData() {
+    const res = await fetch('http://api.weatherapi.com/v1/current.json?key=&q=London&aqi=no', 
+    {
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+    });
+    const data = await res.json();
+    
+    console.log(data);
+
+    weatherLocation.innerHTML = data['location']['name']
+    temperature.innerHTML = data['current']['temp_c']
+    
+}
+
+getWeatherData();
